da9040274ccad0b4cc49925829a48acb62106b69
Juan Daniel González Chico <chicocc15@gmail.com>
2018-11-02 23:01:42 -0600

    He agregado las propiedades de los atributos

--- Proyecto/MiprimerGIT/MiprimerGIT/ClasePokemon.cs
+++ Proyecto/MiprimerGIT/MiprimerGIT/ClasePokemon.cs
@@ -15,10 +15,15 @@ namespace MiprimerGIT
 
         public ClasePokemon(string pokemon, string num, string tipo, string entrenador)
         {
-            this.pokemon = pokemon;
-            this.num = num;
-            this.tipo = tipo;
-            this.entrenador = entrenador;
+            this.Pokemon = pokemon;
+            this.Num = num;
+            this.Tipo = tipo;
+            this.Entrenador = entrenador;
         }
+
+        public string Pokemon { get => pokemon; set => pokemon = value; }
+        public string Num { get => num; set => num = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Entrenador { get => entrenador; set => entrenador = value; }
     }
 }
